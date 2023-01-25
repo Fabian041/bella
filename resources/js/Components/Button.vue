85600eeece992138787a48409f7c701542301373
AIIA\fabian <fabian@aiia.co.id>
2023-01-25 15:58:46 +0630

    initial commit Bella

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: "submit",
+    },
+});
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="inline-flex items-center mt-6 px-5 py-3 bg-emerald-400 border border-transparent rounded-md font-bold text-xs text-white uppercase tracking-widest hover:bg-emerald-500 active:bg-emerald-500 focus:outline-none focus:bg-emerald-500 focus:shadow-outline-emerald transition ease-in-out duration-200"
+    >
+        <slot />
+    </button>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,68 @@
+<script setup>
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import BreezeButton from "@/Components/Button.vue";
+import { Head } from "@inertiajs/inertia-vue3";
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+        <div class="py-24">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="columns-2 gap-20 auto-cols-auto">
+                    <div>
+                        <h1
+                            class="font-medium leading-tight text-5xl mt-0 mb-2 text-slate-900 font-bold inline-block"
+                        >
+                            Booking lebih mudah dengan
+                            <img
+                                class="inline"
+                                src="../../../public/img/bella-fix.png"
+                                alt=""
+                                width="120"
+                            />
+                        </h1>
+                        <p class="pt-1">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Voluptatem non dolorum ipsam fugiat adipisci
+                            doloribus, illum distinctio, officiis temporibus
+                            facilis tempore saepe repellendus eaque quaerat,
+                            ipsa repudiandae placeat commodi dolore!
+                        </p>
+                        <BreezeButton>Show Room</BreezeButton>
+                        <div class="columns-3 gap-5 auto-cols-auto mt-8">
+                            <img
+                                class=""
+                                src="../../../public/img/bella-fix.png"
+                                alt=""
+                                width="50"
+                            />
+                            <p class="pt-1">18 Total Rooms</p>
+                            <img
+                                class=""
+                                src="../../../public/img/bella-fix.png"
+                                alt=""
+                                width="50"
+                            />
+                            <img
+                                class=""
+                                src="../../../public/img/bella-fix.png"
+                                alt=""
+                                width="50"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            class="ml-24 rounded-tl-[160px] rounded-md"
+                            src="../../../public/img/jumbotron.jpg"
+                            alt=""
+                            width="500"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
